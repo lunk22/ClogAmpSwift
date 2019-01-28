8ad462f08de0f32d41a75967a7a0f69d2c4e9bd4
Pascal Roessel <pascal.roessel@sap.com>
2019-01-28 15:26:42 +0100

    Sort by columns + remember column and sort order

--- ClogAmpSwift/ViewController/MainView.swift
+++ ClogAmpSwift/ViewController/MainView.swift
@@ -13,6 +13,9 @@ class MainView: NSViewController {
     var aSongs         = [Song]()
     var aSongsForTable = [Song]()
     
+    var sSortBy        = "title"
+    var bSortAsc       = true
+    
     weak var playerDelegate: PlayerDelegate?
     
     @IBOutlet weak var songTable: NSTableView!
@@ -54,14 +57,15 @@ class MainView: NSViewController {
         self.playerDelegate = self.children[0] as! PlayerView
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
+//    override var representedObject: Any? {
+//        didSet {
+//        // Update the view, if already loaded.
+//        }
+//    }
     
     func setMusicDirectory(_ dir: String){
-        DispatchQueue.main.async{
+        DispatchQueue.global(qos: .background).async {
+         
             self.aSongs = FileSystemUtils.readFolderContentsAsSong(sPath: dir)
             
             if(self.aSongs.count > 0){
@@ -69,18 +73,33 @@ class MainView: NSViewController {
                 
                 self.aSongsForTable = self.aSongs
                 
-                self.sortSongs(by: "title") //refreshs the UI
+                DispatchQueue.main.async {
+                    self.songTable.sortDescriptors = [NSSortDescriptor(key: self.sSortBy, ascending: self.bSortAsc)]
+                }
             }
-            
-            self.directoryLabel.stringValue = dir;
         }
+        
+        self.directoryLabel.stringValue = dir;
+    }
+        
+    func sortSongs(by: String, ascending: Bool){
+        self.sSortBy  = by
+        self.bSortAsc = ascending
+        
+        self.performSortSongs()
     }
     
-    func sortSongs(by: String){
-        self.aSongsForTable.sort(by: { $0.getValueAsString(by).uppercased() < $1.getValueAsString(by).uppercased() })
+    func performSortSongs() {
+        self.aSongsForTable.sort(by: {
+            if(self.bSortAsc){
+                return $0.getValueAsString(self.sSortBy).uppercased() < $1.getValueAsString(self.sSortBy).uppercased()
+            }else{
+                return $0.getValueAsString(self.sSortBy).uppercased() > $1.getValueAsString(self.sSortBy).uppercased()
+            }
+        })
         
         //Update the table view to represent the new sort order
-        DispatchQueue.main.async{
+        DispatchQueue.main.async {
             self.songTable.reloadData()
         }
     }
@@ -90,7 +109,7 @@ class MainView: NSViewController {
         if sender.selectedRow >= 0 {
             self.playerDelegate?.loadSong(song: self.aSongsForTable[sender.selectedRow])
             
-            DispatchQueue.main.async{
+            DispatchQueue.global(qos: .background).async {
                 self.positionTable.reloadData()
             }
         }
@@ -104,9 +123,9 @@ class MainView: NSViewController {
         dialog.showsResizeIndicator    = true;
         dialog.showsHiddenFiles        = false;
         dialog.canChooseDirectories    = true;
+        dialog.canChooseFiles          = false;
         dialog.canCreateDirectories    = false;
         dialog.allowsMultipleSelection = false;
-        dialog.allowedFileTypes        = ["txt"];
 
         if (dialog.runModal() == NSApplication.ModalResponse.OK) {
             if let result = dialog.url { // Pathname of the file
@@ -149,6 +168,14 @@ extension MainView: NSTableViewDelegate, NSTableViewDataSource {
             }
         }
     }
+    
+    func tableView(_ tableView: NSTableView, sortDescriptorsDidChange oldDescriptors: [NSSortDescriptor]) {
+        if(tableView.sortDescriptors.count > 0){
+            let oDesc = tableView.sortDescriptors[0]
+            self.sortSongs(by: oDesc.key!, ascending: oDesc.ascending )
+        }
+        
+    }
 }
 
 extension MainView: NSTextFieldDelegate {
@@ -157,8 +184,7 @@ extension MainView: NSTextFieldDelegate {
     func controlTextDidChange(_ obj: Notification) {
         guard let textField = obj.object as? NSTextField else { return }
         let value = textField.stringValue.lowercased()
-        
-//        let imageObject = questionImageObjects.filter{ $0.imageUUID == imageUUID }.first
+
         if(value != ""){
             self.aSongsForTable = self.aSongs.filter{
                 return
@@ -171,9 +197,10 @@ extension MainView: NSTextFieldDelegate {
             self.aSongsForTable = self.aSongs
         }
         
+        self.performSortSongs()
         
         //Update the table view to represent the new sort order
-        DispatchQueue.main.async{
+        DispatchQueue.main.async {
             self.songTable.reloadData()
         }
     }
