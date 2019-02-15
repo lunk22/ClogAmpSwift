efa3ba03f59b3d04fb1bcc9103dcc2bfb75c8ce8
Pascal Roessel <pascal.roessel@sap.com>
2019-02-15 12:47:30 +0100

    Remember window position and size

---
+++ ClogAmpSwift/Controller/TimePanelWindow.swift
@@ -0,0 +1,19 @@
+//
+//  TimePanelWindow.swift
+//  ClogAmpSwift
+//
+//  Created by Roessel, Pascal on 15.02.19.
+//  Copyright © 2019 Pascal Roessel. All rights reserved.
+//
+
+import Foundation
+
+class TimePanelWindow: NSWindowController {
+    
+    override func windowDidLoad() {
+        window?.setFrameAutosaveName("timeWindowAutosave")
+        
+        super.windowDidLoad()
+    }
+    
+}
