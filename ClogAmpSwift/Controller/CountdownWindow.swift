8f366efc9a236e46224ec66a7b878ff79d5cadd9
Pascal Roessel <pascal.roessel@sap.com>
2019-02-19 09:03:42 +0100

    Added Countdown (CMD+D)

---
+++ ClogAmpSwift/Controller/CountdownWindow.swift
@@ -0,0 +1,19 @@
+//
+//  CountdownWindow.swift
+//  ClogAmpSwift
+//
+//  Created by Roessel, Pascal on 19.02.19.
+//  Copyright © 2019 Pascal Roessel. All rights reserved.
+//
+
+import Foundation
+
+class CountdownWindow: NSWindowController {
+    
+    override func windowDidLoad() {
+        window?.setFrameAutosaveName("countdownWindowAutosave")
+        
+        super.windowDidLoad()
+    }
+    
+}
