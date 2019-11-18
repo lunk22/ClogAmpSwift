d0ee8a0a55e11b484446b97812575468674ab6bc
Pascal Roessel <pascal.roessel@sap.com>
2019-11-18 15:25:46 +0100

    Export History as PDF

--- ClogAmpSwift/Controller/HistoryTableView.swift
+++ ClogAmpSwift/Controller/HistoryTableView.swift
@@ -7,6 +7,7 @@
 //
 
 import AppKit
+import PDFKit
 
 class HistoryTableView: ViewController {
     
@@ -15,7 +16,12 @@ class HistoryTableView: ViewController {
     
     //Outlets
     @IBOutlet weak var historyTable: TableView!
-        
+    
+    //Actions
+    @IBAction func printList(_ sender: Any) {
+        self.createPDF()
+    }
+    
     //Overrides
     override func viewDidLoad() {
 
@@ -24,8 +30,46 @@ class HistoryTableView: ViewController {
 
         historyItems = Database.getSongHistory(nil, to: nil) as? [SongHistoryItem]
         self.historyTable.reloadData()
-                
+        
         super.viewDidLoad()
+    }
+    
+    func createPDF() {
+        if self.historyTable.selectedRow < 0 { return }
+        
+        let header1 = NSLocalizedString("songTitle", bundle: Bundle.main, comment: "") as NSString as String
+        let header2 = NSLocalizedString("songArtist", bundle: Bundle.main, comment: "") as NSString as String
+        let header3 = NSLocalizedString("songDate", bundle: Bundle.main, comment: "") as NSString as String
+        
+        var sPdfHtml        = ""
+        sPdfHtml = sPdfHtml + "<style>"
+        sPdfHtml = sPdfHtml + " th { text-align: left; }"
+        sPdfHtml = sPdfHtml + " table, th, td { border: 1px solid black; border-collapse: collapse; }"
+        sPdfHtml = sPdfHtml + "</style>"
+        sPdfHtml = sPdfHtml + "<br/>"
+        sPdfHtml = sPdfHtml + "<br/>"
+        sPdfHtml = sPdfHtml + "<br/>"
+        sPdfHtml = sPdfHtml + "<br/>"
+        sPdfHtml = sPdfHtml + "<br/>"
+        sPdfHtml = sPdfHtml + "<table style=\"width:100%\">"
+        sPdfHtml = sPdfHtml + " <tr>"
+        sPdfHtml = sPdfHtml + "     <th>\(header1)</th>"
+        sPdfHtml = sPdfHtml + "     <th>\(header2)</th>"
+        sPdfHtml = sPdfHtml + "     <th>\(header3)</th>"
+        sPdfHtml = sPdfHtml + " </tr>"
+        
+        for index in self.historyTable.selectedRowIndexes {
+            if self.historyItems != nil && self.historyItems!.count > index {
+                sPdfHtml = sPdfHtml + " <tr>"
+                sPdfHtml = sPdfHtml + "     <td>\(self.historyItems![index].title ?? "")</td>"
+                sPdfHtml = sPdfHtml + "     <td>\(self.historyItems![index].artist ?? "")</td>"
+                sPdfHtml = sPdfHtml + "     <td>\(self.historyItems![index].date ?? "")</td>"
+                sPdfHtml = sPdfHtml + " </tr>"
+            }
+        }
+        sPdfHtml = sPdfHtml + "</table>"
+        
+        CreatePDF(htmlString: sPdfHtml)
     }
 }
 
