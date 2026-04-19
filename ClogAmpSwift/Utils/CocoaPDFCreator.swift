d372362590b1d1b9e71acee19d3e11629d7d7b4d
Pascal Freundlich <pascal.roessel@gmail.com>
2026-04-19 09:29:24 +0200

    Export History as PDF

---
+++ ClogAmpSwift/Utils/CocoaPDFCreator.swift
@@ -0,0 +1,21 @@
+import Cocoa
+import WebKit
+
+public func CreatePDF(htmlString: String) {
+    let webView = WebView()
+    webView.mainFrame.loadHTMLString(htmlString, baseURL: nil)
+    let when = DispatchTime.now() + 1
+    DispatchQueue.main.asyncAfter(deadline: when) {
+        let printOpts: [NSPrintInfo.AttributeKey : AnyObject] = [NSPrintInfo.AttributeKey.jobDisposition:NSPrintInfo.JobDisposition.save as AnyObject]
+        let printInfo: NSPrintInfo = NSPrintInfo(dictionary: printOpts)
+        printInfo.paperSize = NSMakeSize(595.22, 841.85)
+        printInfo.topMargin = 10.0
+        printInfo.leftMargin = 10.0
+        printInfo.rightMargin = 10.0
+        printInfo.bottomMargin = 10.0
+        let printOp: NSPrintOperation = NSPrintOperation(view: webView.mainFrame.frameView.documentView, printInfo: printInfo)
+        printOp.showsPrintPanel = false
+        printOp.showsProgressPanel = false
+        printOp.run()
+    }
+}
