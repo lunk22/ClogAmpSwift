0c85e04183307cbcfbbbc50e2320aa9ba3e8de4c
Pascal Freundlich <pascal.roessel@gmail.com>
2026-04-19 09:29:19 +0200

    Switched to View Based Tables + Font Size

--- ClogAmpSwift/Controller/PositionTableView.swift
+++ ClogAmpSwift/Controller/PositionTableView.swift
@@ -10,6 +10,8 @@ import AppKit
 
 class PositionTableView: NSViewController {
     
+    var fontSize       = 12
+    
     weak var mainView: MainView?
     
     //Outlets
@@ -24,18 +26,42 @@ class PositionTableView: NSViewController {
     @IBAction func handleSelectPosition(_ sender: NSTableView) {
         self.mainView?.playerView?.handlePositionSelected(sender.selectedRow)
     }
+    
+    @IBAction func handleIncreaseTextSize(_ sender: NSButton) {
+        self.fontSize += 1
+        self.positionTable.reloadData()
+    }
+    @IBAction func handleDecreaseTextSize(_ sender: NSButton) {
+        self.fontSize -= 1
+        self.positionTable.reloadData()
+    }
 }
 
 extension PositionTableView: NSTableViewDelegate, NSTableViewDataSource {
-    
-    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
-        if(tableColumn!.identifier.rawValue == "number"){
-            return row + 1
-        }else if let song = self.mainView?.playerView?.getSong() {
-            return song.positions[row].getValueAsString(tableColumn!.identifier.rawValue)
-        }else{
-            return ""
+
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        if let cell = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: nil) as? NSTableCellView {
+            let textField = cell.textField!
+            let fontDescriptor = textField.font!.fontDescriptor
+            
+            if(tableColumn!.identifier.rawValue == "number"){
+                textField.stringValue = "\(row + 1)"
+            }else if let song = self.mainView?.playerView?.getSong() {
+                textField.stringValue = song.positions[row].getValueAsString(tableColumn!.identifier.rawValue)
+            }else{
+                textField.stringValue = ""
+            }
+            textField.font = NSFont.init(descriptor: fontDescriptor, size: CGFloat(self.fontSize))// .systemFont(ofSize: CGFloat(self.fontSize))
+            textField.sizeToFit()
+            return cell
         }
+        
+        return nil
+    }
+    
+    func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
+        return CGFloat(self.fontSize + 3)
     }
     
     func numberOfRows(in tableView: NSTableView) -> Int {
