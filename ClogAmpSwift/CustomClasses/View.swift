4433acc0650110177619f1945cc86f8e8d32bc53
Pascal Freundlich <pascal.roessel@gmail.com>
2026-04-19 09:29:18 +0200

    Filter + Stability Improvements

---
+++ ClogAmpSwift/CustomClasses/View.swift
@@ -0,0 +1,21 @@
+//
+//  View.swift
+//  ClogAmpSwift
+//
+//  Created by Roessel, Pascal on 21.01.19.
+//  Copyright © 2019 Pascal Roessel. All rights reserved.
+//
+
+import Cocoa
+
+class View: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+    }
+    
+    override func performKeyEquivalent(with event: NSEvent) -> Bool {
+        return true
+    }
+    
+}
--- ClogAmpSwift/ViewController/MainView.swift
+++ ClogAmpSwift/ViewController/MainView.swift
@@ -10,26 +10,60 @@ import AppKit
 
 class MainView: NSViewController {
     
-    var aSongs = [Song]()
+    var aSongs         = [Song]()
+    var aSongsForTable = [Song]()
     
     weak var playerDelegate: PlayerDelegate?
     
     @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var searchField: NSTextField!
     
+    // Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        self.aSongs = FileSystemUtils.readFolderContentsAsSong(sPath: "/Users/pascal/Clogging/musik")
+        self.aSongs = FileSystemUtils.readFolderContentsAsSong(sPath: "/Users/d043610/private/clogging/Musik")
+        self.aSongsForTable = self.aSongs
         self.sortSongs(by: "title")
         
-        DispatchQueue.main.async{
-            self.tableView.reloadData()
+//        NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) {
+//            self.flagsChanged(with: $0)
+//            return $0
+//        }
+
+//        NSEvent.addLocalMonitorForEvents(matching: NSEvent.EventTypeMask.keyDown) {
+//            self.keyDown(with: $0)
+//            return $0
+//        }
+    }
+    
+    override func keyDown(with event: NSEvent) {
+        let keyPressed = (event.charactersIgnoringModifiers ?? "")
+        
+        if(keyPressed == "+"){
+            self.playerDelegate!.increaseSpeed()
+        }else if(keyPressed == "-"){
+            self.playerDelegate!.decreaseSpeed()
+        }else if(keyPressed == "p"){
+            self.playerDelegate!.play()
+        }else if(keyPressed == "s"){
+            self.playerDelegate!.stop()
+        }else if(keyPressed == " "){
+            self.playerDelegate!.pause()
+        }else if(keyPressed == "F"){
+//          if(event.modifierFlags.intersection(.deviceIndependentFlagsMask) == [.shift]){
+            self.searchField.becomeFirstResponder()
+//          }
+        }else{
+            self.interpretKeyEvents([event])
         }
+        
+
     }
     
     override func viewDidAppear() {
-        self.playerDelegate = self.childViewControllers[0] as! PlayerView
+        self.playerDelegate = self.children[0] as! PlayerView
     }
 
     override var representedObject: Any? {
@@ -39,13 +73,18 @@ class MainView: NSViewController {
     }
     
     func sortSongs(by: String){
-        self.aSongs.sort(by: { $0.getValueAsString(by).uppercased() < $1.getValueAsString(by).uppercased() })
+        self.aSongsForTable.sort(by: { $0.getValueAsString(by).uppercased() < $1.getValueAsString(by).uppercased() })
+        
+        //Update the table view to represent the new sort order
+        DispatchQueue.main.async{
+            self.tableView.reloadData()
+        }
     }
     
     //UI Selectors
     @IBAction func songSelected(_ sender: NSTableView) {
         if sender.selectedRow >= 0 {
-            self.playerDelegate?.loadSong(song: aSongs[sender.selectedRow])
+            self.playerDelegate?.loadSong(song: self.aSongsForTable[sender.selectedRow])
         }
     }
     
@@ -54,11 +93,37 @@ class MainView: NSViewController {
 extension MainView: NSTableViewDelegate, NSTableViewDataSource {
     
     func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
-        let value = self.aSongs[row].getValueAsString(tableColumn!.identifier.rawValue)
+        if(self.aSongsForTable.count < (row - 1)){ return "" }
+
+        let value = self.aSongsForTable[row].getValueAsString(tableColumn!.identifier.rawValue)
         return value
     }
     
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return self.aSongs.count
+        return self.aSongsForTable.count
     }
+}
+
+extension MainView: NSTextFieldDelegate {
+
+    func controlTextDidChange(_ obj: Notification) {
+        guard let textField = obj.object as? NSTextField else { return }
+        let value = textField.stringValue.lowercased()
+        
+//        let imageObject = questionImageObjects.filter{ $0.imageUUID == imageUUID }.first
+        if(value != ""){
+            self.aSongsForTable = self.aSongs.filter{
+                return $0.title.lowercased().contains(value)
+            }
+        }else{
+            self.aSongsForTable = self.aSongs
+        }
+        
+        
+        //Update the table view to represent the new sort order
+        DispatchQueue.main.async{
+            self.tableView.reloadData()
+        }
+    }
+    
 }
