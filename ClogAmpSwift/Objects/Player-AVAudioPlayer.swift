56a09ee1d3da7a209062458a47d6b94e1119c8dc
Pascal Freundlich <pascal.roessel@gmail.com>
2026-04-19 09:29:26 +0200

    AVAudioPlayer Test

--- ClogAmpSwift/Objects/Player-AVAudioPlayer.swift
+++ ClogAmpSwift/Objects/Player-AVAudioPlayer.swift
@@ -9,41 +9,41 @@
 //import AppKit
 //import AVFoundation
 //
-//class Player {
-//    
+//class Player: NSObject {
+//
 //    var song: Song
 //    var avPlayer: AVAudioPlayer?
 //    var observer: Any?
 //    var songHistoryWritten: Bool
-//    
+//    var theClosure: (() -> Void)?
+//    var timer: Timer?
+//
 //    init(song: Song) {
 //        self.song = song
-//        AVAudioFormat
+//        self.songHistoryWritten = false
+//
+//        super.init()
+//
 //        do {
-//            self.avPlayer = try AVAudioPlayer(contentsOf: self.song.filePathAsUrl)
+//            self.avPlayer = try AVAudioPlayer(contentsOf: self.song.filePathAsUrl) //, format: AVAudioFormat(settings: settings)
 //            self.avPlayer?.prepareToPlay()
 //            self.avPlayer?.enableRate = true
-////            let settings = [AVAudioTimePitchAlgorithm: AVAudioTimePitchAlgorithm.spectral]
-////                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-////                AVSampleRateKey: 12000,
-////                AVNumberOfChannelsKey: 1,
-////                AVAudioTimePitchAlgorithm: AVAudioTimePitchAlgorithm.spectral
-////                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-////            ]
-//            
-////            self.avPlayer?.format = AVAudioFormat(settings: <#T##[String : Any]#>)
-//            
-////            self.avPlayer?.delegate = self
-////            self.avPlayer.currentItem?.audioTimePitchAlgorithm = AVAudioTimePitchAlgorithm.spectral // High Quality Pitch Algorithm
+//            self.avPlayer?.isMeteringEnabled = true
+//            self.avPlayer?.delegate = self
+//
 //        } catch {}
-//        
-//        self.songHistoryWritten = false
 //    }
-//    
+//
 //    func play() {
+//        if self.theClosure != nil {
+//            self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true){_ in
+//                self.theClosure!()
+//                print(self.getCurrentPower())
+//            }
+//        }
 //        self.avPlayer?.play()
 //        self.updateRate()
-//        
+//
 //        if(!self.songHistoryWritten){
 //            self.songHistoryWritten = true
 //            Database.insertSong(
@@ -53,32 +53,42 @@
 //            )
 //        }
 //    }
-//    
+//
 //    func pause() {
 //        if(self.isPlaying()){
+//            if self.timer != nil {
+//                self.timer!.invalidate()
+//            }
+//
 //            self.avPlayer?.pause()
 //        }else{
 //            self.play()
 //        }
 //    }
-//    
+//
 //    func stop() {
 //        self.avPlayer?.stop()
 //        self.seek(seconds: 0.0){_ in }
+//        if self.timer != nil {
+//            self.timer!.invalidate()
+//        }
+//        if self.theClosure != nil {
+//            self.theClosure!()
+//        }
 //    }
-//    
+//
 //    func isPlaying() -> Bool {
 //        return self.avPlayer?.isPlaying ?? false
 //    }
-//    
+//
 //    func seek(seconds: Float64, using block: @escaping (Bool) -> Void) {
 //        let timescale = CMTimeScale(1)
 //        self.seek(seconds: (seconds * Double(timescale)), timescale: timescale, using: block)
 //    }
-//    
+//
 //    private func seek(seconds: Float64, timescale: CMTimeScale, using block: @escaping (Bool) -> Void) {
 //        self.avPlayer?.currentTime = Double(seconds / Float64(timescale))
-//        
+//
 //        block(true)
 ////        self.avPlayer.seek(
 ////            to: CMTimeMake(value: Int64(lround(seconds)), timescale: timescale),
@@ -87,48 +97,71 @@
 ////            completionHandler: block
 ////        )
 //    }
-//    
+//
 //    func jump(_ seconds: Int) {
-//        
+//
 //        let currentTime = self.getCurrentTime()
 //        let jumpPos = currentTime + Double(seconds)
-//        
+//
 //        self.seek(seconds: jumpPos * 1000, timescale: 1000){_ in }
 //    }
-//    
+//
 //    func updateRate(){
 //        if(self.isPlaying()){
 //            self.avPlayer?.rate = 1.0 + Float(Float(self.song.speed) / 100)
 //        }
 //    }
-//    
+//
 //    func updateVolume() {
 //        self.avPlayer?.volume = Float(self.song.volume) / 100
 //    }
-//    
+//
 //    func getCurrentTime() -> Double {
 //        return self.avPlayer?.currentTime ?? 0
 //    }
-//    
+//
 //    func getDuration() -> Double {
 //        return self.avPlayer?.duration ?? 0
 //    }
-//    
-//    func addPeriodicTimeObserver(using block: @escaping (CMTime) -> Void) {
-////        self.observer = self.avPlayer.addPeriodicTimeObserver(forInterval: CMTimeMake(value: 100, timescale: 1000), queue: nil, using: block)
-//        block(CMTime(value: 0, timescale: 1))
+//
+//    func getCurrentPower() -> Float {
+//        self.avPlayer?.updateMeters()
+//        var averagePowerAcc: Float = 0.0
+//        for i in 0...(self.avPlayer?.numberOfChannels ?? 0)-1 {
+//            averagePowerAcc += self.avPlayer?.averagePower(forChannel: i) ?? 0
+//        }
+//
+//        return averagePowerAcc / Float(self.avPlayer?.numberOfChannels ?? 1)
 //    }
-//    
+//
+//    @objc func songFinished() {
+//        self.stop()
+//        self.theClosure!()
+//        NotificationCenter.default.removeObserver(self)
+//    }
+//
+//    func addPeriodicTimeObserver(using block: @escaping () -> Void) {
+//        self.theClosure = block
+//
+//        NotificationCenter.default.addObserver(self,
+//           selector: #selector(songFinished),
+//           name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+//           object: nil
+//        ) // Add observer
+//    }
+//
 //    func removeTimeObserver() {
-////        if let observer = self.observer {
-////            self.avPlayer.removeTimeObserver(observer)
-////        }
-////        self.observer = nil
+//        self.theClosure = nil
+//        if self.timer != nil {
+//            self.timer!.invalidate()
+//            self.timer = nil
+//        }
 //    }
+//
 //}
 //
-////extension Player: AVAudioPlayerDelegate {
-////    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-////        self.stop()
-////    }
-////}
+//extension Player: AVAudioPlayerDelegate {
+//    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+//        self.songFinished()
+//    }
+//}
